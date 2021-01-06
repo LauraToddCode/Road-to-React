c9d7ba6ff04d0d4d7b8d8e77a150fc1d3160e577
LauraToddCode <laura20todd@gmail.com>
2021-01-06 11:45:22 +0000

    Using useState("") function to read input

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -22,8 +22,10 @@ const App = () => {
     }
   ]
 
+  const [searchTerm, setSearchTerm] = React.useState("")
+
   const handleChange = event => {
-    console.log(event.target.value)
+    setSearchTerm(event.target.value)
   }
 
   return (
@@ -33,6 +35,9 @@ const App = () => {
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" onChange={handleChange} />
 
+      <p>
+        Searching for <strong>{searchTerm}</strong>
+      </p>
       <hr />
 
       <List list={stories}/>
