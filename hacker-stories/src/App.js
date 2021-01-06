52beaf94785611c1f0fb7845fc666f69db2eaef0
LauraToddCode <laura20todd@gmail.com>
2021-01-06 11:34:03 +0000

    Mapped the list using props

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -1,25 +1,27 @@
 import React from "react"
 
-const list = [
-  {
-    title: "React",
-    url: "https://reactjs.org/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  }
-]
+
 
 const App = () => {
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    }
+  ]
+
   const handleChange = event => {
     console.log(event.target.value)
   }
@@ -33,13 +35,13 @@ const App = () => {
 
       <hr />
 
-      <List />
+      <List list={stories}/>
     </div>
   );
 }
 
-const List = () => {
-  return list.map(item => {
+const List = props => {
+  return props.list.map(item => {
     return (
       <div key={item.objectID}>
         <span>
