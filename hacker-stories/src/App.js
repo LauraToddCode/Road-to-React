21952ba329716124ef564f42293d559f40133790
LauraToddCode <laura20todd@gmail.com>
2021-02-06 10:32:45 +0000

    React Fragments
    Changed wrapping HTML elements in Search component to empty HTML tags so as not to introduce intermediate elements in our rendered HTML

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -63,15 +63,16 @@ const App = () => {
 
 const Search = ({search, onSearch}) => (
 
-    <div>
+    <>
       <label htmlFor="search">Search: </label>
       <input 
         id="search" 
         type="text" 
         value={search}
-        onChange={onSearch} />
+        onChange={onSearch} 
+      />
 
-    </div>
+    </>
 )
 
 const List = ({ list }) => 
