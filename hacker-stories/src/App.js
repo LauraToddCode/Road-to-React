14d96767c4caa7f900e72d1373de20b313411274
LauraToddCode <laura20todd@gmail.com>
2021-04-25 14:04:10 +0100

    Added a remove button and inline handler

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -1,43 +1,52 @@
 import React from "react"
 
+const initialStories = [
+  {
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  }
+]
 
+const useSemiPersistentState = (key, initialState) => {
+  const [value, setValue] = React.useState(
+    localStorage.getItem(key) || initialState
+  )
+
+  React.useEffect(() => {
+    localStorage.setItem(key, value)
+  }, [value, key])
+
+  return [value, setValue]
+}
 
 const App = () => {
-  const stories = [
-    {
-      title: "React",
-      url: "https://reactjs.org/",
-      author: "Jordan Walke",
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    },
-    {
-      title: "Redux",
-      url: "https://redux.js.org/",
-      author: "Dan Abramov, Andrew Clark",
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    }
-  ]
-
-  const useSemiPersistentState = (key, initialState) => {
-    const [value, setValue] = React.useState(
-      localStorage.getItem(key) || initialState
-    )
   
-    React.useEffect(() => {
-      localStorage.setItem(key, value)
-    }, [value, key])
-
-    return [value, setValue]
-  }
-
   const [searchTerm, setSearchTerm] = useSemiPersistentState(
     "search",
     "React"
   )
+
+  const [stories, setStories] = React.useState(initialStories);
+
+  const handleRemoveStory = item => {
+    const newStories = stories.filter(
+      story => item.objectID !== story.objectID
+    )
+
+    setStories(newStories);
+  };
 
   const handleSearch = event => {
     setSearchTerm(event.target.value)
@@ -56,12 +65,13 @@ const App = () => {
       <InputWithLabel
         id="search"
         value={searchTerm}
+        isFocused
         onInputChange={handleSearch}
       >
         <strong>Search:</strong>  
       </InputWithLabel>
 
-      <List list={searchedStories}/>
+      <List list={searchedStories} onRemoveItem={handleRemoveStory} />
     </div>
   );
 }
@@ -71,35 +81,61 @@ const InputWithLabel = ({
   value, 
   type = "text",
   onInputChange,
+  isFocused,
   children 
-}) => (
+  }) => {
 
-    <>
-      <label htmlFor={id}>{children}</label>
-      &nbsp;
-      <input 
-        id={id} 
-        type={type} 
-        value={value}
-        autofocus
-        onChange={onInputChange} 
-      />
+    const inputRef = React.useRef();
 
-    </>
-)
+    React.useEffect(() => {
+      if (isFocused && inputRef.current) {
+        inputRef.current.focus();
+      }
+    }, [isFocused]);
+    
+    return (
+      <>
+        <label htmlFor={id}>{children}</label>
+        &nbsp;
+        <input 
+          ref={inputRef}
+          id={id} 
+          type={type} 
+          value={value}
+          autofocus={isFocused}
+          onChange={onInputChange} 
+        />
 
-const List = ({ list }) => 
-  list.map(item => <Item key={item.objectID} item={item} />)
-
-const Item = ({ item }) => (
-      <div key={item.objectID}>
-        <span>
-          <a href={item.url}>{item.title}</a>
-        </span>
-        <span>{item.author}</span>
-        <span>{item.num_comments}</span>
-        <span>{item.points}</span>
-      </div>
+      </>
     )
+  }
+
+const List = ({ list, onRemoveItem }) => 
+  list.map(item => 
+    <Item 
+      key={item.objectID} 
+      item={item} 
+      onRemoveItem={onRemoveItem}
+    />
+  )
+
+const Item = ({ item, onRemoveItem }) => {
+
+  return (
+    <div key={item.objectID}>
+      <span>
+        <a href={item.url}>{item.title}</a>
+      </span>
+      <span>{item.author}</span>
+      <span>{item.num_comments}</span>
+      <span>{item.points}</span>
+      <span>
+        <button type="button" onClick={() => onRemoveItem(item)}>
+          Dismiss
+        </button>
+      </span>
+    </div>
+  )
+}
 
 export default App;
